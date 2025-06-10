4692e00419dfc63fcbee84c09385160317dbe3b7
YuJoong <yu@YuJoong-MacBook.local>
2025-06-10 17:25:00 +0900

    create alb use for querypie

---
+++ querypie/modules/alb/alb.tf
@@ -0,0 +1,35 @@
+resource "aws_lb" "this" {
+  name               = "querypie-alb"
+  load_balancer_type = "application"
+  internal           = false
+
+  security_groups    = [var.alb_sg_id]
+  subnets            = var.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "qp-alb"
+  }
+}
+
+resource "aws_lb_target_group" "qp_tg" {
+  name     = "querypie-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTPS"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "qp-tg"
+  }
+}
