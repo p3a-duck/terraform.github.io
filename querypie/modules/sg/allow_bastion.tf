1b3e034751d337e0ff9d56bd4f88d7fcc94c032b
paaaduck <paaaduck@gmail.com>
2025-06-18 11:11:22 +0900

    Add base architecture for QueryPie (v0.3)

--- querypie/modules/sg/allow_bastion.tf
+++ querypie/modules/sg/allow_bastion.tf
@@ -2,12 +2,27 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_bastion"
   description = "Allow SSH access to the bastion server"
   vpc_id      = var.vpc_id
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_ssh_justin"
   }
 }
 
+/*
 resource "aws_vpc_security_group_ingress_rule" "allow_22" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -24,3 +39,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_443" {
   ip_protocol       = "tcp"
   to_port           = 443
 }
+
+#egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+}
+*/
--- querypie/modules/sg/allow_querypie.tf
+++ querypie/modules/sg/allow_querypie.tf
@@ -2,12 +2,41 @@ resource "aws_security_group" "allow_querypie" {
   name        = "allow_querypie"
   description = "Allow Querypie inbound traffic and all outbound traffic"
   vpc_id      = var.vpc_id
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_querypie_justin"
   }
 }
 
+/*
 resource "aws_vpc_security_group_ingress_rule" "allow_querypie_ssh" {
   security_group_id = aws_security_group.allow_querypie.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -47,3 +76,12 @@ resource "aws_vpc_security_group_ingress_rule" "allow_querypie_icmp" {
   to_port           = -1
   ip_protocol       = "icmp"
 }
+
+#egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_all" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "-1"
+  to_port           = 0
+}*/
