f2f3f43635c02c03174c3d2ccc7c0034c638fbdd
paaaduck <paaaduck@gmail.com>
2025-06-19 11:38:11 +0900

    Add base architecture for QueryPie (v0.4)

--- querypie/modules/sg/allow_bastion.tf
+++ querypie/modules/sg/allow_bastion.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "allow_ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
